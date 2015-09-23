34c35245e967e5fb2600262f51897dbc442dc52c
John Jablonski <jablonski.kce@gmail.com>
2015-09-23 18:51:31 +0200

    Added some debugging, but issue not resolved.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,16 @@ package controllers
 
 import play.api._
 import play.api.libs.EventSource
-import play.api.libs.iteratee.Concurrent
+import play.api.libs.iteratee.{Iteratee, Concurrent}
 import play.api.libs.json.JsValue
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits._
 
 class Application extends Controller {
 
   val (chatOut, chatChannel) = Concurrent.broadcast[JsValue]
+  val chatDebug = Iteratee.foreach[JsValue](m => println("Debug: " + m.toString))
+  chatOut |>>> chatDebug
 
   def index = Action { implicit req =>
     Ok(views.html.index(routes.Application.chatFeed(), routes.Application.postMessage()))
@@ -16,7 +19,9 @@ class Application extends Controller {
 
   def chatFeed = Action { req =>
     println("User connected to chat: " + req.remoteAddress)
-    Ok.chunked(chatOut &> EventSource()).as("text/event-stream")
+    Ok.chunked(chatOut
+      &> EventSource()
+    ).as("text/event-stream")
   }
 
   def postMessage = Action(parse.json) { req =>
